c55ff79c361c45e3759a8d50faf3c7f385a27cfe
juniorsilvacc <juniorsilvacc@hotmail.com>
2023-09-19 01:04:39 -0300

    organizando views

--- resources/views/admin/supports/edit.blade.php
+++ resources/views/admin/supports/edit.blade.php
@@ -1,18 +1,10 @@
 <h1>Editar Dúvida: {{ $support->id }} </h1>
 
-@if ($errors->any())
-    @foreach ($errors->all() as $error)
-        {{ $error }}
-    @endforeach
-@endif
+<x-alert/>
 
 <form action="{{ route('supports.editAction', $support->id) }}" method="POST">
-    <input type="hidden" value={{ csrf_token() }} name="_token">
-
-    {{-- <input type="text" value="PUT" name="_method"> --}}
     @method('PUT')
-
-    <input type="text" placeholder="Assunto" name="subject" value="{{ $support->subject }}">
-    <textarea name="body" cols="30" rows="5" placeholder="Descrição">{{ $support->body }}</textarea>
-    <button type="submit">Atualizar</button>
+    @include('admin.supports.partials.form', [
+        'support' => $support
+    ])
 </form>
